41a948475bab9e6f10eea6c5814295b2ba1f8a17
Gnoah2512 <106863842+Gnoah2512@users.noreply.github.com>
2025-04-30 20:25:03 +0700

    hotfix cookie options

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,7 @@ function getCookieOptions() {
   return {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 }
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN,
+    origin: [process.env.CLIENT_ORIGIN, "http://localhost:3000"],
     credentials: true,
   })
 );
