803c13fbf902ae064f845153173dbb1070bff4a5
Gnoah2512 <106863842+Gnoah2512@users.noreply.github.com>
2025-04-30 16:35:26 +0700

    railway testing

--- server/index.js
+++ server/index.js
@@ -40,5 +40,5 @@ app.use("/admin", adminRouter);
 app.use("/deliverer", delivererRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
